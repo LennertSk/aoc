47ec7f4d046db7f7622efd4d5ca87f65eaaa8a53
Lennertsk <lennert.slabbinck@iodigital.com>
2025-12-12 09:25:51 +0100

    2025: 3

---
+++ src/2025/3/puzzle.ts
@@ -0,0 +1,43 @@
+const first = (input: string) => {
+  let score1 = 0;
+  let score2 = 0;
+  
+  for (const bank of input.split('\n')) {
+    const bankArr = Array.from(bank).filter(x => /[1-9]/.test(x));
+    score1 += biggestJoltage(bankArr);
+    score2 += biggestJoltage2(bankArr)
+  }
+
+  return "first solution: " + score2;
+};
+
+const second = (input: string) => {};
+
+const biggestJoltage = (bank: Array<string>) => {
+  let biggy: number = Number(bank[0] + '' + bank[1]);
+  for (let i = 0; i < bank.length - 1; i++) {
+    for (let x = i + 1; x < bank.length; x++) {
+      const temp: number = Number(bank[i] + '' + bank[x]);
+      if (temp > biggy) {
+        biggy = temp;
+      }
+      if (biggy == 99) {
+        return biggy;
+      }
+    }
+  }
+  return biggy;
+}
+
+const biggestJoltage2 = (bank: Array<string>) => {
+  const retVal: Array<string> = [];
+  let index: number = 0;
+  while (retVal.length < 12 && index < bank.length) {
+    const bankSlice = bank.slice(index, bank.length - (11 - retVal.length)).sort((a, b) => Number(b) - Number(a));
+    retVal.push(bankSlice[0]);
+    index = bank.indexOf(bankSlice[0], index) + 1;
+  }
+  return Number(retVal.join(''));
+}
+
+export { first, second };
